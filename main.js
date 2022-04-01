8f9cbe5b1929d662ec54a36164cdea9db31f60e0
liadk07 <91607695+liad07@users.noreply.github.com>
2022-04-01 14:25:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+if (window.matchMedia('(prefers-color-scheme: dark)').media === 'not all') {
+    document.documentElement.style.display = 'none';
+    document.head.insertAdjacentHTML(
+        'beforeend',
+        '<link rel="stylesheet" href="/light.css" onload="document.documentElement.style.display = \'\'">',
+    );
+
+}
